c719c4e15452ae87aea01e05200bcb636d1c3aba
Karthik Jagadeesha <kartu8@gmail.com>
2017-11-02 21:59:11 +0530

    Add Brush Size Change feature

--- painter/sketch.js
+++ painter/sketch.js
@@ -1,31 +1,54 @@
-function onBodyLoad() {
-  document.querySelector('#toolbar').remove();
-}
-
 let global = window;
+let brushWidth = 5,
+  brushHeight = 5;
+
+const canvasBackColor = "#fff";
+
+//utilities
+const createBrushStroke = () => {
+  noStroke();
+  fill("red");
+  ellipse(mouseX, mouseY, brushWidth, brushHeight);
+};
+
+const clearCanvas = () => {
+  background(canvasBackColor);
+};
 
 // instantiation - runs for the first time
 function setup() {
   createCanvas(global.innerWidth - 100, global.innerHeight - 50);
-  background("#fff");
+  clearCanvas();
 }
 
 // Draw loop - runs for every frame
+// print 'frameRate' to check for the current frame
 function draw() {}
 
 // User Interactive events
 function mouseDragged() {
-  noStroke();
-  fill("red");
-  ellipse(mouseX, mouseY, 5, 5);
+  createBrushStroke();
 }
 
 function mouseClicked() {
-  noStroke();
-  fill("red");
-  ellipse(mouseX, mouseY, 5, 5);
+  createBrushStroke();
 }
 
 function onClearClick() {
-  background("#fff");
+  clearCanvas();
+}
+
+function onBrushSizeChange(event) {
+  const size = event.target.value;
+  switch (size) {
+    case "1":
+      (brushWidth = 5), (brushHeight = 5);
+      break;
+    case "2":
+      (brushWidth = 15), (brushHeight = 15);
+      break;
+    case "3":
+      (brushWidth = 30), (brushHeight = 30);
+      break;
+  }
 }
