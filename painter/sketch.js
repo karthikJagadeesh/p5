81514bbccf1f4dfef0fd559d522f7df742640740
Karthik Jagadeesh <kartu8@gmail.com>
2017-11-02 18:25:04 +0530

    Add paint canvas and clear canvas

--- painter/sketch.js
+++ painter/sketch.js
@@ -1,9 +1,31 @@
+function onBodyLoad() {
+  document.querySelector('#toolbar').remove();
+}
+
+let global = window;
+
 // instantiation - runs for the first time
 function setup() {
-  
+  createCanvas(global.innerWidth - 100, global.innerHeight - 50);
+  background("#fff");
 }
 
 // Draw loop - runs for every frame
-function draw() {
-  
+function draw() {}
+
+// User Interactive events
+function mouseDragged() {
+  noStroke();
+  fill("red");
+  ellipse(mouseX, mouseY, 10, 10);
+}
+
+function mouseClicked() {
+  noStroke();
+  fill("red");
+  ellipse(mouseX, mouseY, 10, 10);
+}
+
+function onClearClick() {
+  background("#fff");
 }
