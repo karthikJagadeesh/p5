93d8e6e9b8d25b43ec16396c7c6c02112ca35eab
Karthik Jagadeesh <kartu8@gmail.com>
2017-11-06 17:14:33 +0530

    Code refactor

--- painter/sketch.js
+++ painter/sketch.js
@@ -1,120 +1,151 @@
-let global = window;
-let brushSize = 1;
-let brushColor = "#851934";
-let currentPaintColor = brushColor;
-let redDotInPainter = document.querySelector("#red-dot-painter");
-redDotInPainter.style.background = currentPaintColor;
+function app(p) {
+  let global = window;
+  let brush = {};
+  let redDotInPainter = document.querySelector("#red-dot-painter");
 
-const canvasBackColor = "#fff";
+  class Brush {
+    constructor() {
+      this.type = "brush";
+      this.size = 2;
+      this.color = "#851934";
+      this.cacheColor = this.color;
+    }
 
-//utilities
-const createBrushStroke = () => {
-  noFill();
-  stroke(brushColor);
-  strokeWeight(brushSize);
-  curve(
-    pmouseX,
-    pmouseY,
-    pmouseX,
-    pmouseY,
-    mouseX,
-    mouseY,
-    mouseX,
-    mouseY
-  );
-};
+    doBrushStroke() {
+      p.noFill();
+      p.stroke(this.color);
+      p.strokeWeight(this.size);
+      p.curve(
+        p.pmouseX,
+        p.pmouseY,
+        p.pmouseX,
+        p.pmouseY,
+        p.mouseX,
+        p.mouseY,
+        p.mouseX,
+        p.mouseY
+      );
+    }
 
-const clearCanvas = () => {
-  background(canvasBackColor);
-};
+    changeBrushSize(event) {
+      const size = event.target.value;
+      switch (size) {
+        case "2":
+          this.size = 2;
+          break;
+        case "4":
+          this.size = 6;
+          break;
+        case "6":
+          this.size = 10;
+          break;
+        case "8":
+          this.size = 20;
+          break;
+        case "10":
+          this.size = 40;
+          break;
+      }
+    }
 
-// instantiation - runs for the first time
-function setup() {
-  createCanvas(global.innerWidth - 100, global.innerHeight - 100);
-  clearCanvas();
-  smooth();
-}
+    changeBrushType(event) {
+      this.type = event.target.dataset.value;
+      if (this.type === "eraser") {
+        this.color = settings.canvasBackColor;
+      } else if (this.type === "brush") {
+        this.color = this.cacheColor;
+      }
+    }
 
-// Draw loop - runs for every frame
-// print 'frameRate' to check for the current frame
-function draw() {}
-
-function windowResized() {
-  resizeCanvas(global.innerWidth - 100, global.innerHeight - 100);
-}
-
-// User Interactive events
-function mousePressed() {
-  createBrushStroke();
-}
-
-function mouseDragged() {
-  createBrushStroke();
-}
-
-function onClearClick() {
-  clearCanvas();
-}
-
-function onBrushSizeChange(event) {
-  const size = event.target.value;
-  switch (size) {
-    case "2":
-      brushSize = 2;
-      break;
-    case "4":
-      brushSize = 6;
-      break;
-    case "6":
-      brushSize = 10;
-      break;
-    case "8":
-      brushSize = 20;
-      break;
-    case "10":
-      brushSize = 40;
-      break;
+    changeBrushColor(event) {
+      const color = event.target.dataset.value;
+      switch (color) {
+        case "red":
+          this.color = "#851934";
+          break;
+        case "green":
+          this.color = "#1F8A70";
+          break;
+        case "yellow":
+          this.color = "#FFE11A";
+          break;
+        case "blue":
+          this.color = "#165873";
+          break;
+        default:
+          this.color = event.target.value;
+      }
+      this.cacheColor = redDotInPainter.style.background = this.color;
+    }
   }
+
+  const settings = {
+    canvasBackColor: "#fff",
+    redDotInPainterColor: brush.color,
+    onClearClick: () => {
+      clearCanvas();
+    },
+    onScreenSizeToggle: event => {
+      const isFullScreen = event.target.checked;
+      p.fullscreen(isFullScreen);
+    }
+  };
+
+  //utilities
+  const clearCanvas = () => {
+    p.background(settings.canvasBackColor);
+  };
+
+  // p5 setup - runs for the first time
+  p.setup = () => {
+    brush = new Brush();
+    p.createCanvas(global.innerWidth - 100, global.innerHeight - 100);
+    clearCanvas();
+    p.smooth();
+    redDotInPainter.style.background = settings.redDotInPainterColor;
+  };
+
+  // Draw loop - runs for every frame
+  // print 'frameRate' to check for the current frame
+  p.draw = () => {};
+
+  // User Interactive events
+  p.mousePressed = () => {
+    brush.doBrushStroke();
+  };
+
+  p.mouseDragged = () => {
+    brush.doBrushStroke();
+  };
+
+  p.windowResized = () => {
+    p.resizeCanvas(global.innerWidth - 100, global.innerHeight - 100);
+  };
+
+  let quickPalette = ["#red", "#green", "#yellow", "#blue"];
+
+  quickPalette.forEach(color => {
+    document.querySelector(color).onclick = event =>
+      brush.changeBrushColor(event);
+  });
+
+  document.querySelector("#color-picker").oninput = event =>
+    brush.changeBrushColor(event);
+  document.querySelector("#brush-size").onchange = event =>
+    brush.changeBrushSize(event);
+  document.querySelector("#clear-canvas").onclick = settings.onClearClick;
+  document.querySelector("#toggle-screen").onclick = settings.onScreenSizeToggle;
+
+  document.querySelector("#brush").onclick = event => {
+    document.body.style.cursor =
+      "url(http://cur.cursors-4u.net/user/use-1/use151.cur), default";
+    brush.changeBrushType(event);
+  };
+  document.querySelector("#eraser").onclick = event => {
+    document.body.style.cursor =
+      "url(./images/eraser-cursor-icon.png), default";
+    brush.changeBrushType(event);
+  };
 }
 
-function onBrushColorChangeClick(event) {
-  const color = event.target.dataset.value;
-  switch (color) {
-    case "red":
-      currentPaintColor = brushColor = "#851934";
-      redDotInPainter.style.background = currentPaintColor;
-      break;
-    case "green":
-      currentPaintColor = brushColor = "#1F8A70";
-      redDotInPainter.style.background = currentPaintColor;
-      break;
-    case "yellow":
-      currentPaintColor = brushColor = "#FFE11A";
-      redDotInPainter.style.background = currentPaintColor;
-      break;
-    case "blue":
-      currentPaintColor = brushColor = "#165873";
-      redDotInPainter.style.background = currentPaintColor;
-      break;
-  }
-}
-
-function colorPickerInputChanged(event) {
-  const color = event.target.value;
-  currentPaintColor = brushColor = color;
-  redDotInPainter.style.background = currentPaintColor;
-}
-
-function onScreenSizeToggle(event) {
-  const isFullScreen = event.target.checked;
-  fullscreen(isFullScreen);
-}
-
-function chooseBrushType(event) {
-  const brushType = event.target.dataset.value;
-  if (brushType === "eraser") {
-    brushColor = canvasBackColor;
-  } else if (brushType === "brush") {
-    brushColor = currentPaintColor;
-  }
-}
+var appInstance = new p5(app);
--- particle-system/sketch.js
+++ particle-system/sketch.js
@@ -1,89 +1,89 @@
-let global = window;
-let bubbles = [];
-let numberOfBubbles = 20;
-let x, y, r, c, id;
+  // let global = window;
+  // let bubbles = [];
+  // let numberOfBubbles = 20;
+  // let x, y, r, c, id;
 
-//utilities
-const checkForEdgeClash = bubble => {
-  if (bubble.positionX > width || bubble.positionX < 0) {
-    bubble.speedX = bubble.speedX * -1;
-  } else if (bubble.positionY > height || bubble.positionY < 0) {
-    bubble.speedY = bubble.speedY * -1;
-  }
-};
+  // //utilities
+  // const checkForEdgeClash = bubble => {
+  //   if (bubble.positionX > width || bubble.positionX < 0) {
+  //     bubble.speedX = bubble.speedX * -1;
+  //   } else if (bubble.positionY > height || bubble.positionY < 0) {
+  //     bubble.speedY = bubble.speedY * -1;
+  //   }
+  // };
 
-const distanceBetweenTwoBubbles = (bubbleA, bubbleB) =>
-  dist(
-    bubbleA.positionX,
-    bubbleA.positionY,
-    bubbleB.positionX,
-    bubbleB.positionY
-  );
+  // const distanceBetweenTwoBubbles = (bubbleA, bubbleB) =>
+  //   dist(
+  //     bubbleA.positionX,
+  //     bubbleA.positionY,
+  //     bubbleB.positionX,
+  //     bubbleB.positionY
+  //   );
 
-const checkForCollision = bubble => {
-  bubbles.forEach(bubbleFromAllList => {
-    const distance = distanceBetweenTwoBubbles(bubbleFromAllList, bubble);
-    if (
-      distance < bubbleFromAllList.radius / 2 + bubble.radius / 2 &&
-      bubbleFromAllList.id !== bubble.id
-    ) {
-      bubble.color = [random(255), random(255), random(255)];
-      bubble.radius = random(15, 25);
-    }
-  });
-};
+  // const checkForCollision = bubble => {
+  //   bubbles.forEach(bubbleFromAllList => {
+  //     const distance = distanceBetweenTwoBubbles(bubbleFromAllList, bubble);
+  //     if (
+  //       distance < bubbleFromAllList.radius / 2 + bubble.radius / 2 &&
+  //       bubbleFromAllList.id !== bubble.id
+  //     ) {
+  //       bubble.color = [random(255), random(255), random(255)];
+  //       bubble.radius = random(15, 25);
+  //     }
+  //   });
+  // };
 
-class Bubble {
-  constructor(positionX, positionY, radius, color, id) {
-    this.positionX = positionX;
-    this.positionY = positionY;
-    this.radius = radius * 2;
-    this.color = color;
-    this.speedX = random(-6, 5);
-    this.speedY = random(-5, 6);
-    this.id = id;
-  }
+  // class Bubble {
+  //   constructor(positionX, positionY, radius, color, id) {
+  //     this.positionX = positionX;
+  //     this.positionY = positionY;
+  //     this.radius = radius * 2;
+  //     this.color = color;
+  //     this.speedX = random(-6, 5);
+  //     this.speedY = random(-5, 6);
+  //     this.id = id;
+  //   }
 
-  show() {
-    noStroke();
-    fill(...this.color);
-    ellipse(this.positionX, this.positionY, this.radius);
-  }
+  //   show() {
+  //     noStroke();
+  //     fill(...this.color);
+  //     ellipse(this.positionX, this.positionY, this.radius);
+  //   }
 
-  move() {
-    this.positionX = this.positionX + this.speedX;
-    this.positionY = this.positionY + this.speedY;
-  }
-}
+  //   move() {
+  //     this.positionX = this.positionX + this.speedX;
+  //     this.positionY = this.positionY + this.speedY;
+  //   }
+  // }
 
-// instantiation - runs for the first time
-function setup() {
-  createCanvas(global.innerWidth - 100, global.innerHeight - 100);
-  smooth();
-  background(255);
+  // // instantiation - runs for the first time
+  // function setup() {
+  //   createCanvas(global.innerWidth - 100, global.innerHeight - 100);
+  //   smooth();
+  //   background(255);
 
-  for (let i = 0; i < numberOfBubbles; i++) {
-    x = random(width);
-    y = random(height);
-    r = random(15, 25);
-    c = [random(255), random(255), random(255)];
-    id = i;
-    bubbles.push(new Bubble(x, y, r, c, id));
-  }
-}
+  //   for (let i = 0; i < numberOfBubbles; i++) {
+  //     x = random(width);
+  //     y = random(height);
+  //     r = random(15, 25);
+  //     c = [random(255), random(255), random(255)];
+  //     id = i;
+  //     bubbles.push(new Bubble(x, y, r, c, id));
+  //   }
+  // }
 
-// Draw loop - runs for every frame
-// print 'frameRate' to check for the current frame
-function draw() {
-  // background(0);
-  bubbles.forEach(bubble => {
-    checkForEdgeClash(bubble);
-    checkForCollision(bubble);
-    bubble.show();
-    bubble.move();
-  });
-}
+  // // Draw loop - runs for every frame
+  // // print 'frameRate' to check for the current frame
+  // function draw() {
+  //   // background(0);
+  //   bubbles.forEach(bubble => {
+  //     checkForEdgeClash(bubble);
+  //     checkForCollision(bubble);
+  //     bubble.show();
+  //     bubble.move();
+  //   });
+  // }
 
-function windowResized() {
-  resizeCanvas(global.innerWidth - 100, global.innerHeight - 100);
-}
+  // function windowResized() {
+  //   resizeCanvas(global.innerWidth - 100, global.innerHeight - 100);
+  // }
