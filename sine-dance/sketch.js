2161c01bda659054780459bb9439fa02512f0b27
Karthik Jagadeesh <kartu8@gmail.com>
2017-11-09 00:03:50 +0530

    Add size slider - control ball size using the slider

--- sine-dance/sketch.js
+++ sine-dance/sketch.js
@@ -4,7 +4,8 @@ class Ball {
   constructor(posX, posY, radius, offsetX, i) {
     this.posX = posX;
     this.posY = posY;
-    this.diameter = radius * 2;
+    this.originalDiameter = radius * 2;
+    this.diameter = this.originalDiameter;
     this.speedY = 0.1;
     this.speedX = 0;
     this.offsetX = offsetX;
@@ -13,6 +14,10 @@ class Ball {
 
   set speed(speed) {
     this.offsetX = (this.i + 1) / speed;
+  }
+
+  set size(size) {
+    this.diameter = this.originalDiameter + size;
   }
 
   show() {
@@ -41,7 +46,7 @@ function setup() {
   for (let i = 0; i < noOfBalls; i++) {
     let offsetX = (i + 1) / 1000;
     let posYOffset = totalOffset * (i + 0.5);
-    balls.push(new Ball(width / 2, posYOffset, 5, offsetX, i));
+    balls.push(new Ball(width / 2, posYOffset, 2, offsetX, i));
   }
 }
 
@@ -53,7 +58,7 @@ function draw() {
   });
 }
 
-const changeBallSize = event => {
+const changeBallSpeed = event => {
   const value = event.target.value;
   switch (value) {
     case "1":
@@ -109,7 +114,67 @@ const changeBallSize = event => {
   }
 };
 
+const changeBallSize = event => {
+  const value = event.target.value;
+  switch (value) {
+    case "1":
+      balls.forEach(ball => {
+        ball.size = 0;
+      });
+      break;
+    case "2":
+      balls.forEach(ball => {
+        ball.size = 2;
+      });
+      break;
+    case "3":
+      balls.forEach(ball => {
+        ball.size = 4;
+      });
+      break;
+    case "4":
+      balls.forEach(ball => {
+        ball.size = 6;
+      });
+      break;
+    case "5":
+      balls.forEach(ball => {
+        ball.size = 8;
+      });
+      break;
+    case "6":
+      balls.forEach(ball => {
+        ball.size = 10;
+      });
+      break;
+    case "7":
+      balls.forEach(ball => {
+        ball.size = 12;
+      });
+      break;
+    case "8":
+      balls.forEach(ball => {
+        ball.size = 14;
+      });
+      break;
+    case "9":
+      balls.forEach(ball => {
+        ball.size = 16;
+      });
+      break;
+    case "10":
+      balls.forEach(ball => {
+        ball.size = 18;
+      });
+      break;
+  }
+};
+
 //User Events
 document
   .querySelector("#speed-slider")
+  .addEventListener("input", changeBallSpeed);
+
+document
+  .querySelector("#size-slider")
   .addEventListener("input", changeBallSize);
